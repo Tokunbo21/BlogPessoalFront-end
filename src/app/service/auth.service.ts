6bdc3be9baaee593e470b2927c5286c1a9f1c7b7
unknown <tofadetokunbo@hotmail.com>
2021-06-30 19:40:53 -0300

    Componete inicio sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,6 +2,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -15,11 +16,21 @@ export class AuthService {
     ) { }
    
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('https://blogpessoaltokunbo.herokuapp.com/usuarios/logar', userLogin)
-
+    return this.http.post<UserLogin>('https://blogpessoaltoks.herokuapp.com/usuarios/logar', userLogin)
+                     
   }
 
   cadastrar(user: User) : Observable<User> { 
-    return this.http.post<User>(' https://blogpessoaltokunbo.herokuapp.com/usuarios/cadastrar', user)
+    return this.http.post<User>(' https://blogpessoaltoks.herokuapp.com/usuarios/cadastrar', user)
+  }
+
+  logado() {
+    let ok: boolean = false
+
+    if(environment.token !=''){
+      ok = true
+    }
+
+    return ok 
   }
 }
